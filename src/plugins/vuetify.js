e19bfb81bf3c0548162b7b97aabf897a4a4f96f1
jbienesdev <jbienes.dev@gmail.com>
2020-10-23 15:54:30 +0800

    chore: remove unnecessary code

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,16 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify);
+import colors from 'vuetify/lib/util/colors'
 
-export default new Vuetify({});
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.amber.darken2
+      }
+    }
+  }
+})
