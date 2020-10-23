64d2c7c8ad97e6691503c8423936587b5a5e83ec
jbienesdev <jbienes.dev@gmail.com>
2020-10-23 17:00:59 +0800

    feat: todo list interface

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="d-flex justify-space-between pa-4 mb-2">
+    <div class="d-flex align-center">
+      <v-icon style="cursor: pointer;">
+        mdi-drag-vertical
+      </v-icon>
+      <p class="text-body-1 mb-0 ml-4">{{ title }}</p>
+    </div>
+    <div>
+      <v-btn class="mx-2" fab dark x-small color="success">
+        <v-icon dark>
+          mdi-check
+        </v-icon>
+      </v-btn>
+      <v-btn class="mx-2" fab dark x-small color="error">
+        <v-icon dark>
+          mdi-trash-can
+        </v-icon>
+      </v-btn>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    status: {
+      type: String,
+      default: 'todo'
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,42 @@
 <template>
-  <div class="home">
-    <v-container>
-      This is the home page.
-    </v-container>
+  <div style="max-width: 720px;" class="home mx-auto">
+    <v-card class="d-flex align-center mt-12 pa-4">
+      <v-text-field
+        prepend-inner-icon="mdi-clipboard-outline"
+        class="mr-4"
+        label="Enter a task..."
+      ></v-text-field>
+      <v-btn class="mx-2" fab dark small color="primary">
+        <v-icon dark>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+    </v-card>
+    <v-content>
+      <todo-item
+        v-for="item in items"
+        :key="item.id"
+        :title="item.title"
+        :status="item.status"
+      ></todo-item>
+    </v-content>
   </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      items: [
+        { id: 1, title: 'Sample todo', status: 'todo' },
+        { id: 2, title: 'Sample todo 2', status: 'todo' }
+      ]
+    }
+  }
 }
 </script>
