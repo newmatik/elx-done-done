15309587a445716f450e48d5cc3bf25bf16bf872
jbienesdev <jbienes.dev@gmail.com>
2020-10-24 14:50:38 +0800

    feat: create app logo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="transparent" elevation="0">
       <v-spacer></v-spacer>
-      <h1>Done&amp;Done</h1>
+      <img src="images/DoneDoneLogo.svg" alt="Done Done Logo" />
       <v-spacer></v-spacer>
     </v-app-bar>
 
