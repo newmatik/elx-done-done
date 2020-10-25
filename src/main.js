b8294daba59e5ca25f7b85681f7e3323042b8b8a
jbienesdev <jbienes.dev@gmail.com>
2020-10-25 13:04:35 +0800

    chore(package.json): remove vuefire library

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import { firestorePlugin } from 'vuefire'
-
-Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
