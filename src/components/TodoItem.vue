8b9a3a157bd559873f754d1e66d182c8a7498a0f
jbienesdev <jbienes.dev@gmail.com>
2020-10-26 14:43:09 +0800

    feat(todoitem): show prompt when deleting a task

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -6,7 +6,7 @@
     outlined
   >
     <div style="width: 80%" class="d-flex align-center">
-      <div :style="{ visibility: type === 'headline' ? 'hidden' : 'visible' }">
+      <div v-if="type !== 'headline'">
         <v-checkbox
           class="ma-0 pa-0"
           :input-value="status === 'done'"
@@ -18,8 +18,11 @@
         <v-text-field
           v-if="isEditing"
           hide-details
-          class="ma-0 pa-0 ml-4"
-          :class="{ 'font-weight-black': type === 'headline' }"
+          class="ma-0 pa-0"
+          :class="{
+            'font-weight-black': type === 'headline',
+            'ml-4': type === 'task'
+          }"
           :value="taskTitle"
           ref="task"
           @blur="editTask"
@@ -27,10 +30,11 @@
         ></v-text-field>
         <p
           v-else
-          class="text-body-1 mb-0 ml-4"
+          class="text-body-1 mb-0"
           :class="{
             strikethrough: status === 'done',
-            'font-weight-black': type === 'headline'
+            'font-weight-black': type === 'headline',
+            'ml-4': type === 'task'
           }"
           @click="onTaskClick"
         >
@@ -38,8 +42,8 @@
         </p>
       </div>
     </div>
-    <div class="d-flex">
-      <v-btn @click="removeTask" class="mx-2" fab x-small elevation="0">
+    <div class="d-flex align-center">
+      <v-btn @click="dialog = true" class="mx-2" fab x-small elevation="0">
         <v-icon>
           mdi-trash-can
         </v-icon>
@@ -48,6 +52,27 @@
         mdi-drag-vertical
       </v-icon>
     </div>
+
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline red--text">
+          Deleting {{ type }}...
+        </v-card-title>
+        <v-card-text>
+          Are you sure you want to delete this {{ type }}? This action cannot be
+          undone.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog = false">
+            Cancel
+          </v-btn>
+          <v-btn color="red darken-1" dark @click="removeTask">
+            Delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -76,7 +101,8 @@ export default {
   },
   data() {
     return {
-      isEditing: false
+      isEditing: false,
+      dialog: false
     }
   },
   methods: {
