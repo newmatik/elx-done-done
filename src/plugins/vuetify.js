bc5ef3db97728d3f1773d8b39c6e7fd0386b4261
jbienesdev <jbienes.dev@gmail.com>
2020-11-04 10:50:58 +0800

    style: change app logo and primary color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
-import colors from 'vuetify/lib/util/colors'
-
 Vue.use(Vuetify)
 
 export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.amber.darken2
+        primary: '#9b1915'
       }
     }
   }
